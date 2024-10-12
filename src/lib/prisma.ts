8e5e7737d12700a78ad4890006b34b4aa6b02489
Carlos Azaustre <cazaustre@gmail.com>
2024-10-12 12:15:34 +0200

    fix: :rotating_light: fix linting errors

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,3 +1,4 @@
+// eslint-disable @typescript-eslint/no-unsafe-member-access
 import { PrismaClient } from '@prisma/client';
 
 let prisma: PrismaClient;
@@ -5,9 +6,12 @@ let prisma: PrismaClient;
 if (process.env.NODE_ENV === 'production') {
   prisma = new PrismaClient();
 } else {
+  // @ts-expect-error: Unreachable code error
   if (!global.prisma) {
+    // @ts-expect-error: Unreachable code error
     global.prisma = new PrismaClient();
   }
+  // @ts-expect-error: Unreachable code error
   prisma = global.prisma;
 }
 
