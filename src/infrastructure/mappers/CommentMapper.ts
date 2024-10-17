ffee4b0d3bf183aa82302a0c51948c791f372035
Carlos Azaustre <cazaustre@gmail.com>
2024-10-17 20:34:15 +0200

    feat: add sanitization on form input fields

--- src/infrastructure/mappers/CommentMapper.ts
+++ src/infrastructure/mappers/CommentMapper.ts
@@ -3,6 +3,18 @@ import { CommentRow } from '@/infrastructure/database/types';
 import { CommentDTO } from '@/core/dtos/CommentDTO';
 
 export class CommentMapper {
+  static toDomain(row: CommentRow, conversationId: number): Comment {
+    return {
+      id: row.id,
+      content: row.content,
+      createdAt: new Date(row.created_at),
+      updatedAt: row.updated_at ? new Date(row.updated_at) : null,
+      userId: row.user_id,
+      username: row.user_username,
+      conversationId,
+    };
+  }
+
   static toDTO(comment: Comment): CommentDTO {
     return {
       id: comment.id,
@@ -15,19 +27,6 @@ export class CommentMapper {
         id: comment.userId,
         username: comment.username || 'Usuario Anónimo',
       },
-    };
-  }
-
-  // eslint-disable-next-line @typescript-eslint/no-explicit-any
-  static toDomain(row: CommentRow, conversationId: number): Comment {
-    return {
-      id: row.id,
-      content: row.content,
-      createdAt: new Date(row.created_at),
-      updatedAt: row.updated_at ? new Date(row.updated_at) : null,
-      userId: row.user_id,
-      username: row.user_username,
-      conversationId,
     };
   }
 }
